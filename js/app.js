4b55083837a1ec93b3521e28b59ad47ecbed9713
thegioiwebviet.vn <thegioiwebviet.vn@gmail.com>
2023-10-09 16:46:21 +0700

    commit 3

--- js/app.js
+++ js/app.js
@@ -9,12 +9,70 @@ const saleSwiper = new Swiper('.sale-swiper', {
     }
 })
 
-const prodcutSwiper = new Swiper('.vegetable .product-swiper', {
+const vegetableProductSwiper = new Swiper('.vegetable-product-swiper', {
     direction: 'horizontal',
-    slidesPerView: 5,
+    slidesPerView: 4,
+    spaceBetween: 15,
     loop: false,
     navigation: {
         nextEl: '.vegetable-swiper-next',
         prevEl: '.vegetable-swiper-prev',
+    }
+})
+
+const banner2Swiper = new Swiper('.banner-2-swiper', {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 15,
+    loop: false,
+})
+
+const dryfoodProductSwiper = new Swiper('.dryfood-product-swiper', {
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 15,
+    loop: false,
+    navigation: {
+        nextEl: '.dryfood-swiper-next',
+        prevEl: '.dryfood-swiper-prev',
+    }
+})
+
+const juiceProductSwiper = new Swiper('.juice-product-swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    loop: false,
+    navigation: {
+        nextEl: '.juice-swiper-next',
+        prevEl: '.juice-swiper-prev',
+    }
+})
+const saladProductSwiper = new Swiper('.salad-product-swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    loop: false,
+    navigation: {
+        nextEl: '.salad-swiper-next',
+        prevEl: '.salad-swiper-prev',
+    }
+})
+const otherProductSwiper = new Swiper('.other-product-swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    loop: false,
+    navigation: {
+        nextEl: '.other-swiper-next',
+        prevEl: '.other-swiper-prev',
+    }
+})
+
+const newsSwiper = new Swiper('.news-swiper', {
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 15,
+    loop: false,
+    navigation: {
+        nextEl: '.news-swiper-next',
+        prevEl: '.news-swiper-prev',
     }
 })
